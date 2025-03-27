ccc35b3a23cf60b5b64af5e8771ae4db310c3795
ajaxlaur <ajaxlaur@132.161.196.222>
2025-03-27 15:00:36 -0500

    Mostly done.

--- src/main/java/edu/grinnell/csc207/soundsofsorting/sortevents/CompareEvent.java
+++ src/main/java/edu/grinnell/csc207/soundsofsorting/sortevents/CompareEvent.java
@@ -1,9 +1,39 @@
 package edu.grinnell.csc207.soundsofsorting.sortevents;
 
+import java.util.List;
+import java.util.ArrayList;
+
 /**
  * A <code>CompareEvent</code> logs a comparison a sort makes between two
  * indices in the array.
+ * @param <T>
  */
-public class CompareEvent<T> {
-    // TODO: implement me!
+public class CompareEvent<T extends Comparable <T>> implements SortEvent<T> {
+    
+    private T[] newArr;
+    private int firstIndex;
+    private int secondIndex;
+
+    public CompareEvent(T[] array, int ind1, int ind2) {
+        newArr = array;
+        firstIndex = ind1;
+        secondIndex = ind2;
+    }
+    
+    @Override
+    public void apply(T[] arr) { }
+
+    @Override
+    public List<Integer> getAffectedIndices() {
+        List<Integer> list = new ArrayList<>();
+        list.add(firstIndex);
+        list.add(secondIndex);
+        return list;
+    }
+
+    @Override
+    public boolean isEmphasized() {
+        return false;
+    }
+    
 }
