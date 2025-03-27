1c5f977226ce42e2886bf5fb5a3f18521c3047bd
ajaxlaur <ajaxlaur@132.161.196.222>
2025-03-27 15:01:07 -0500

    Mostly done.

--- src/main/java/edu/grinnell/csc207/soundsofsorting/sortevents/SwapEvent.java
+++ src/main/java/edu/grinnell/csc207/soundsofsorting/sortevents/SwapEvent.java
@@ -1,8 +1,40 @@
 package edu.grinnell.csc207.soundsofsorting.sortevents;
 
+import java.util.List;
+import java.util.ArrayList;
+
 /**
  * A <code>SwapEvent</code> logs a swap between two indices of the array.
+ * @param <T>
  */
-public class SwapEvent<T> {
-    // TODO: implement me
+public class SwapEvent<T> implements SortEvent<T> {
+
+    private int firstIndex;
+    private int secondIndex;
+
+    public SwapEvent(int ind1, int ind2) {
+        firstIndex = ind1;
+        secondIndex = ind2;
+    }
+    
+    @Override
+    public void apply(T[] arr) {
+        T tmp = arr[firstIndex];
+        arr[firstIndex] = arr[secondIndex];
+        arr[secondIndex] = tmp;
+    }
+
+    @Override
+    public List<Integer> getAffectedIndices() {
+        List<Integer> list = new ArrayList<>();
+        list.add(firstIndex);
+        list.add(secondIndex);
+        return list;
+    }
+
+    @Override
+    public boolean isEmphasized() {
+        return true;
+    }
+    
 }
